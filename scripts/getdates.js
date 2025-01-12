5a74cf8b6f52653b2812733a1d9b7be6bd473f47
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-12 01:15:42 -0700

    Saving work

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,8 @@
-alert(document.lastModified)
+const date = new Date;
+const year = document.getElementById("copyright-year");
+
+year.innerHTML += date.getFullYear();
+
+const modified = document.getElementById("lastModified");
+let lastModified = new Date(document.lastModified);
+modified.innerHTML += lastModified;
