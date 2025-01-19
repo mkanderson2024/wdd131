7ceb5c7b47209aacc08a5da8d854c6c21c1f037f
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-19 15:16:56 -0700

    saving changes to work

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,11 @@ year.innerHTML += date.getFullYear();
 const modified = document.getElementById("lastModified");
 let lastModified = new Date(document.lastModified);
 modified.innerHTML += lastModified;
+
+const hambutton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
