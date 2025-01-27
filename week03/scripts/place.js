8beb1ae0eddaabcb7535b39a688e47c3dbad7224
mkanderson2024 <kalani2014.ma@gmail.com>
2025-01-26 17:22:19 -0700

    saving progress

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,8 +1,41 @@
+//Copyright Year Calculation
 const date = new Date;
 const year = document.getElementById("copyright-year");
-
 year.innerHTML += date.getFullYear();
-
+//Last Modified Calculation
 const modified = document.getElementById("lastModified");
 let lastModified = new Date(document.lastModified);
 modified.innerHTML += lastModified;
+
+//Windchill Calculation
+//Constants
+const temp = 90;
+const speed = 6;
+
+//Query selectors
+
+const temperature = document.querySelector("#temperature");
+const condition = document.querySelector("#conditions");
+const wind = document.querySelector("#wind");
+const windchill = document.querySelector("#windchill");
+
+
+//If statment
+if (temp <= 50 && speed > 3) {
+    const windChillTemp = calcWindChill(temp, speed);
+    temperature.innerHTML = `${temp}`;
+    condition.innerHTML = `Partly Cloudy`;
+    wind.innerHTML = `${speed} mph`
+    windchill.innerHTML = `${windChillTemp}`
+}
+else {
+    temperature.innerHTML = `${temp}`;
+    condition.innerHTML = `Partly Cloudy`;
+    wind.innerHTML = `${speed} mph`
+    windchill.innerHTML = `N/A`
+}
+
+//Function
+function calcWindChill(u, v) {
+    return Math.sqrt(Math.pow(u, 2) + Math.pow(v, 2));
+}
