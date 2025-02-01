555a0f4147c4023f68da7cf782fe1aa5c984a353
mkanderson2024 <kalani2014.ma@gmail.com>
2025-02-01 11:11:25 -0700

    Saving work

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,167 @@
+const date = new Date;
+const year = document.getElementById("copyright-year");
+
+year.innerHTML += date.getFullYear();
+
+const modified = document.getElementById("lastModified");
+let lastModified = new Date(document.lastModified);
+modified.innerHTML += lastModified;
+
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Brigham City Utah",
+        location: "Brigham City, Utah, United States",
+        dedicated: "2012, September, 23",
+        area: 36000,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/brigham-city-utah-temple/brigham-city-utah-temple-39612-main.jpg"
+    },
+    {
+        templeName: "San Diego California",
+        location: "San Diego, California, United States",
+        dedicated: "1993, April, 30",
+        area: 72000,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/san-diego-california-temple/san-diego-california-temple-9060-main.jpg"
+    },
+    {
+        templeName: "Logan Utah",
+        location: "Logan, Utah, United States",
+        dedicated: "1884, April, 30",
+        area: "119619",
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/logan-utah-temple/logan-utah-temple-40550-main.jpg"
+    },
+    // Add more temple objects here...
+];
+
+createTempleCard(temples);
+
+const homeLink = document.querySelector("#home")
+homeLink.addEventListener("click", () => {
+    document.querySelector(".pictures-container").innerHTML = "";
+    createTempleCard(temples);
+    document.getElementById("h2").innerHTML = "Home";
+})
+
+const oldLink = document.querySelector("#old")
+oldLink.addEventListener("click", () => {
+    document.querySelector(".pictures-container").innerHTML = "";
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1888")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1884")))
+    document.getElementById("h2").innerHTML = "Old: Built before 1900";
+})
+
+const largeLink = document.querySelector("#large")
+largeLink.addEventListener("click", () => {
+    document.querySelector(".pictures-container").innerHTML = "";
+    createTempleCard(temples.filter(temple => temple.area.includes("2005")))
+    document.getElementById("h2").innerHTML = "Large";
+})
+
+
+const newLink = document.querySelector("#new")
+newLink.addEventListener("click", () => {
+    document.querySelector(".pictures-container").innerHTML = "";
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("2005")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("2015")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("2020")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1974")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1986")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1983")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("2012")))
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1993")))
+    document.getElementById("h2").innerHTML = "New: Built after 1990";
+})
+function createTempleCard(filteredTemples) {
+    filteredTemples.forEach(temple => {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+
+        name.textContext = temple.templeName;
+        location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+        dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+        area.innerHTML = `<span class="lable">Size:</span> ${temple.area} sq ft`;
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", `${temple.templeName} Temple`)
+        img.setAttribute("loading", "lazy");
+        img.setAttribute("width", "200");
+        img.setAttribute("height", "150");
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        document.querySelector(".pictures-container").appendChild(card);
+    });
+}
