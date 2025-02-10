d5446b02806d48135a871dbd309f22128669fa6c
mkanderson2024 <kalani2014.ma@gmail.com>
2025-02-10 12:05:59 -0700

    saving progress

---
+++ scripts/project.js
@@ -0,0 +1,45 @@
+//-------Copyright Date Script-------
+const date = new Date;
+const year = document.getElementById("copyright-year");
+
+year.innerHTML += date.getFullYear();
+
+//Last Modified Script
+const modified = document.getElementById("lastModified");
+let lastModified = new Date(document.lastModified);
+modified.innerHTML += lastModified;
+
+//-------Hamburger Button Script -------
+
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+//--------Title Change During Screensize Change-------
+const mediaQuery = window.matchMedia('(max-width: 1200px)');
+
+function handleMediaChange(event) {
+    if (event.matches) {
+        // Actions to perform when the media query matches (e.g., screen is 700 px or less)
+        console.log('Changes Title To IEC');
+        const h1Element = document.querySelector('h1');
+        // Change the text using textContent
+        h1Element.textContent = 'IEC';
+    } else {
+        // Actions to perform when the media query does not match (e.g., screen is larger than 768px)
+        console.log('Changes Title to Interplanetary Enforcemnet Corps');
+        const h1Element = document.querySelector('h1');
+        // Change the text using textContent
+        h1Element.textContent = 'Interplanetary Enforcemnet Corps';
+    }
+}
+
+// Attach the event listener
+mediaQuery.addEventListener('change', handleMediaChange);
+
+// Initial check (optional, to run the function on page load)
+handleMediaChange(mediaQuery);
